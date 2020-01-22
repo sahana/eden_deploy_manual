95b9ea31810364e39ce0e604f040d71dbdf71580
Fran Boon <fran@aidiq.com>
2020-01-22 08:50:42 +0000

    MySQL PyDAL: Fix the fix

--- install-eden-nginx-postgis.sh
+++ install-eden-nginx-postgis.sh
@@ -156,6 +156,7 @@ git submodule update --init --recursive
 
 # Fix for 2.18.5
 sed -i "s|if getattr(func, 'validate', None) is Validator.validate:|if getattr(func, 'validate', None) is not Validator.validate:|" $WEB2PY_HOME/gluon/packages/dal/pydal/validators.py
+sed -i "s|\['password'\]|['passwd']|" /home/web2py/gluon/packages/dal/pydal/adapters/mysql.py
 
 ## Create symbolic link in /root
 ln -s $WEB2PY_HOME ~
